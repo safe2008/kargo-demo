dc44cb9b26739d6db8d567b9a28f33e41809e235
Kargo Render <kargo-render@akuity.io>
2024-02-23 14:39:48 +0000

    updated stages/uat/kustomization.yaml to use image nginx:1.25.4

---
+++ k3d.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -x
+
+k3d cluster create kargo-quickstart \
+  --no-lb \
+  --k3s-arg '--disable=traefik@server:0' \
+  -p '31443-31444:31443-31444@servers:0:direct' \
+  -p '30081-30083:30081-30083@servers:0:direct' \
+  --wait
+
+helm install cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --version 1.14.2 \
+  --namespace cert-manager \
+  --create-namespace \
+  --set installCRDs=true \
+  --wait
+
+helm install argocd argo-cd \
+  --repo https://argoproj.github.io/argo-helm \
+  --version 6.2.3 \
+  --namespace argocd \
+  --create-namespace \
+  --set 'configs.secret.argocdServerAdminPassword=$2a$10$5vm8wXaSdbuff0m9l21JdevzXBzJFPCi8sy6OOnpZMAG.fOXL7jvO' \
+  --set dex.enabled=false \
+  --set notifications.enabled=false \
+  --set server.service.type=NodePort \
+  --set server.service.nodePortHttp=31443 \
+  --set server.extensions.enabled=true \
+  --set 'server.extensions.contents[0].name=argo-rollouts' \
+  --set 'server.extensions.contents[0].url=https://github.com/argoproj-labs/rollout-extension/releases/download/v0.3.4/extension.tar' \
+  --wait
+
+helm install argo-rollouts argo-rollouts \
+  --repo https://argoproj.github.io/argo-helm \
+  --version 2.34.3 \
+  --create-namespace \
+  --namespace argo-rollouts \
+  --wait
+
+helm install kargo \
+  oci://ghcr.io/akuity/kargo-charts/kargo \
+  --namespace kargo \
+  --create-namespace \
+  --set api.service.type=NodePort \
+  --set api.service.nodePort=31444 \
+  --set api.adminAccount.password=admin \
+  --set api.adminAccount.tokenSigningKey=iwishtowashmyirishwristwatch \
+  --wait
